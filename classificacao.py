18881a58efa3662b68bbe197eb662c1bfdf84ba1
Adilson Jose dos Santos <adilson.santos@agu.gov.br>
2022-04-23 02:25:52 -0300

    Calcula a taxa de acerto do algoritmo

--- classificacao.py
+++ classificacao.py
@@ -11,23 +11,35 @@ cachorro3 = [0, 1, 1]
 
 dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
 
-marcacoes = [1, 1, 1, -1, -1, -1]  # Marcamos 1 para porco e -1 para cachorro
+marcacoes = [1, 1, 1, -1, -1, -1]  # Marcamos (1 para porco) e (-1 para cachorro)
 
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
 
-elementoMisterioso1 = [[1, 1, 1]]
-elementoMisterioso2 = [[1, 0, 0]]
-elementoMisterioso3 = [[0, 0, 1]]
+elementoMisterioso1 = [1, 1, 1]           # 1º Animal a ser analisado pelo sistema
+elementoMisterioso2 = [1, 0, 0]           # 2º Animal a ser analisado pelo sistema
+elementoMisterioso3 = [0, 0, 1]           # 3º Animal a ser analisado pelo sistema
 
-teste = (elementoMisterioso1, elementoMisterioso2, elementoMisterioso3)
+teste = [elementoMisterioso1, elementoMisterioso2, elementoMisterioso3]
 
-marcacoes_teste = [-1, 1, -1]
+marcacoes_teste = [-1, 1, -1]              # Resultado esperado
 
-resultado = modelo.predict(teste)
+resultado = modelo.predict(teste)          # Resultado calculado pelo sistema
 
 print(resultado)
 
+diferencas = resultado - marcacoes_teste   # Diferença entre o que foi esperado e o resultado do sistema
 
+print(diferencas)                          # Zero corresponde a cada acerto
+
+acertos = [d for d in diferencas if d==0]
+
+print(acertos)
+
+total_de_acertos = len(acertos)             # Calculamos o total de acertos
+total_de_elementos = len(teste)             # Calculamos o total elementos testados
+
+print("O total de acertos foi", total_de_acertos, "de um total de ",total_de_elementos, "elementos testados")
+print("A taxa de acerto foi de ", total_de_acertos / total_de_elementos * 100, "%")
 
 
